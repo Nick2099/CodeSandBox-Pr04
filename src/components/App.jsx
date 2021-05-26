f5b2b755be0b6ba4095ec3cc849914f0b95708be
Nikica Dadic <75340386+Nick2099@users.noreply.github.com>
2021-05-26 22:29:52 +0200

    Adding JSX components

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// importing a Heading function from Heading.jsx
+// extension doesn't have to be written
+import Heading from "./Heading";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Heading></Heading>
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,10 @@
+// following line is necessary for every React component
+// so it's possible to write JSX code
+import React from "react";
+
+function Heading() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+export default Heading;
+// allows to export and use in other programs this component
